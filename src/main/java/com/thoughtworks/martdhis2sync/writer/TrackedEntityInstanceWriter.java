e2ec90f77df4acfdfa4eb5ac24b38155234e9fda
Pratibhasagar V <pratibav@thoughtworks.com>
2018-08-23 17:10:39 +0530

    Pratibha, Mahitha | #37 | Make API call to sync to DHIS2 server.

--- src/main/java/com/thoughtworks/martdhis2sync/writer/TrackedEntityInstanceWriter.java
+++ src/main/java/com/thoughtworks/martdhis2sync/writer/TrackedEntityInstanceWriter.java
@@ -1,12 +1,21 @@
 package com.thoughtworks.martdhis2sync.writer;
 
+import com.thoughtworks.martdhis2sync.repository.SyncRepository;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
 public class TrackedEntityInstanceWriter implements ItemWriter {
+
+    @Value("${tei.uri}")
+    private String teiUri;
+
+    @Autowired
+    private SyncRepository syncRepository;
 
     @Override
     public void write(List list) throws Exception {
@@ -15,8 +24,7 @@ public class TrackedEntityInstanceWriter implements ItemWriter {
         list.stream().forEach(item -> instanceApiFormat.append(item + ","));
 
         instanceApiFormat.replace(instanceApiFormat.length() - 1, instanceApiFormat.length(), "]}");
+
+        syncRepository.sendData(teiUri, instanceApiFormat.toString());
     }
 }
-
-
-
