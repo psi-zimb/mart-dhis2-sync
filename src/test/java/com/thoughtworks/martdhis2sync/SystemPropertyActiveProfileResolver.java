0b2f299af3fb9cf948c7a6fea7f225fd59283585
Mahitha Arveti <mahitha.arveti@gmail.com>
2018-08-27 17:49:24 +0530

    Mahitha | #37 | Added integration test

---
+++ src/test/java/com/thoughtworks/martdhis2sync/SystemPropertyActiveProfileResolver.java
@@ -0,0 +1,17 @@
+package com.thoughtworks.martdhis2sync;
+
+import org.springframework.test.context.ActiveProfilesResolver;
+import org.springframework.test.context.support.DefaultActiveProfilesResolver;
+
+public class SystemPropertyActiveProfileResolver implements ActiveProfilesResolver {
+
+    private final DefaultActiveProfilesResolver defaultActiveProfilesResolver = new DefaultActiveProfilesResolver();
+
+    @Override
+    public String[] resolve(Class<?> testClass) {
+
+        return System.getProperties().containsKey("spring.profiles.active") ?
+                System.getProperty("spring.profiles.active").split("\\s*,\\s*") :
+                defaultActiveProfilesResolver.resolve(testClass);
+    }
+}
