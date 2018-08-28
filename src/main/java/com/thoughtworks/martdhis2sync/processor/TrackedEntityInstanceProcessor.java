50c3ae1f6f69374ea3f62dc657436e07afc43fdf
Pratibhasagar V <pratibav@thoughtworks.com>
2018-08-28 17:31:55 +0530

    Pratibha, Pavan | #41 | Check for existing TEI UID. Add it to the TEI sync request body to send to DHIS2.

--- src/main/java/com/thoughtworks/martdhis2sync/processor/TrackedEntityInstanceProcessor.java
+++ src/main/java/com/thoughtworks/martdhis2sync/processor/TrackedEntityInstanceProcessor.java
@@ -3,6 +3,7 @@ package com.thoughtworks.martdhis2sync.processor;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.thoughtworks.martdhis2sync.util.TEIUtil;
 import lombok.Setter;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.beans.factory.annotation.Value;
@@ -33,17 +34,22 @@ public class TrackedEntityInstanceProcessor implements ItemProcessor {
         JsonObject tableRowJsonObject = tableRowJsonElement.getAsJsonObject();
         JsonObject mappingJsonObject = mappingObjJsonElement.getAsJsonObject();
 
+        TEIUtil.setPatientIds(tableRowJsonObject);
+
         Set<String> keys = tableRowJsonObject.keySet();
 
-        StringBuilder attributeSet = new StringBuilder(String.format("{\"trackedEntityType\": \"%s\", " +
-                "\"trackedEntityInstance\": \"\", " +
-                "\"orgUnit\":\"%s\"," +
-                "\"attributes\":[", teUID, orgUnitUID));
-        for(String key: keys) {
-            String attribute = mappingJsonObject.get(key).toString();
-            String value = tableRowJsonObject.get(key).toString();
-            if (!EMPTY_STRING.equals(attribute)) {
-                attributeSet.append(String.format("{\"attribute\": %s, \"value\": %s},", attribute, value));
+        StringBuilder attributeSet = new StringBuilder(
+                String.format("{\"trackedEntityType\": \"%s\", " +
+                                "\"trackedEntityInstance\": %s, " +
+                                "\"orgUnit\":\"%s\", \"attributes\":[",
+                        teUID, tableRowJsonObject.get("instance_id").toString(), orgUnitUID));
+        for (String key : keys) {
+            if (null != mappingJsonObject.get(key)) {
+                String attribute = mappingJsonObject.get(key).toString();
+                String value = tableRowJsonObject.get(key).toString();
+                if (!EMPTY_STRING.equals(attribute)) {
+                    attributeSet.append(String.format("{\"attribute\": %s, \"value\": %s},", attribute, value));
+                }
             }
         }
 
---
+++ src/main/java/com/thoughtworks/martdhis2sync/util/TEIUtil.java
@@ -0,0 +1,29 @@
+package com.thoughtworks.martdhis2sync.util;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import lombok.Getter;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class TEIUtil {
+
+    @Getter
+    private static Map<String, String> patientIdTEIUidMap = new LinkedHashMap<>();
+
+    private static String jsonToString(JsonElement jsonElement) {
+        if (jsonElement == null) {
+            return "";
+        } else {
+            return jsonElement.toString();
+        }
+    }
+
+    public static void setPatientIds(JsonObject tableRowJsonObject) {
+        patientIdTEIUidMap.put(
+                jsonToString(tableRowJsonObject.get("Patient_Identifier")),
+                jsonToString(tableRowJsonObject.get("instance_id"))
+        );
+    }
+}
