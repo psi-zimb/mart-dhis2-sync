242f3ee37b996de66096f60e8dc67ed1e68d0d46
Pratibhasagar V <pratibav@thoughtworks.com>
2018-08-29 18:53:30 +0530

    Pratibha | #41 | Add Log4j2 logs, if OrgUnitUID is incorrect, DHIS2 returns StatusCode SUCCESS with conflicts in ImportSummary.

--- src/main/java/com/thoughtworks/martdhis2sync/response/ImportSummary.java
+++ src/main/java/com/thoughtworks/martdhis2sync/response/ImportSummary.java
@@ -2,10 +2,17 @@ package com.thoughtworks.martdhis2sync.response;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class ImportSummary {
+
+    public static final String RESPONSE_SUCCESS = "SUCCESS";
+    public static final String RESPONSE_CONFLICT = "CONFLICT";
+
     String responseType;
     String status;
     ImportCount importCount;
+    List<Conflict> conflicts;
     String reference;
 }
