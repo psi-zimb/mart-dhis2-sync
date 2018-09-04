7723e63657ea932384291cbd06100651eb3d8578
Mahitha Arveti <mahitha.arveti@gmail.com>
2018-09-04 11:06:06 +0530

    Mahitha | #000 | Added PowerMockIgnore annotation to avoid unrecognized JMX error while running unit tests

--- src/test/java/com/thoughtworks/martdhis2sync/reader/MappingReaderTest.java
+++ src/test/java/com/thoughtworks/martdhis2sync/reader/MappingReaderTest.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.slf4j.Logger;
@@ -28,6 +29,7 @@ import static org.powermock.api.mockito.PowerMockito.whenNew;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({MappingReader.class, BatchUtil.class})
+@PowerMockIgnore("javax.management.*")
 public class MappingReaderTest {
 
     @Mock
