090b64e935cc49d4975cf7bbf61cb6368576f073
Pratibhasagar V <pratibav@thoughtworks.com>
2018-09-10 12:18:00 +0530

    Pratibha | ### | Remove OrgUnit from env. variables. Reset marker date before the Job starts.

--- src/main/java/com/thoughtworks/martdhis2sync/step/TrackedEntityInstanceStep.java
+++ src/main/java/com/thoughtworks/martdhis2sync/step/TrackedEntityInstanceStep.java
@@ -2,12 +2,15 @@ package com.thoughtworks.martdhis2sync.step;
 
 import com.thoughtworks.martdhis2sync.processor.TrackedEntityInstanceProcessor;
 import com.thoughtworks.martdhis2sync.reader.MappingReader;
+import com.thoughtworks.martdhis2sync.util.TEIUtil;
 import com.thoughtworks.martdhis2sync.writer.TrackedEntityInstanceWriter;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 
 @Component
@@ -26,6 +29,7 @@ public class TrackedEntityInstanceStep {
     private TrackedEntityInstanceWriter writer;
 
     public Step get(String lookupTable, Object mappingObj, String programName) {
+        TEIUtil.date = new Date(Long.MIN_VALUE);
         return stepBuilderFactory.get("TrackedEntityInstanceStep")
                 .chunk(500)
                 .reader(mappingReader.get(lookupTable, programName))
