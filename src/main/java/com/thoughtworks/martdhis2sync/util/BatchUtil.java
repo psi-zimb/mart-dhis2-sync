44d9baab596204bf1ba9bd1c0e574a84dc0642e5
Pratibhasagar V <pratibav@thoughtworks.com>
2018-09-12 12:03:29 +0530

    Pratibha | ### | Change the table columns from 'created_date' to 'date_created'.

--- src/main/java/com/thoughtworks/martdhis2sync/util/BatchUtil.java
+++ src/main/java/com/thoughtworks/martdhis2sync/util/BatchUtil.java
@@ -4,11 +4,24 @@ import org.apache.commons.io.IOUtils;
 import org.springframework.core.io.Resource;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class BatchUtil {
+
+    private static final String DATEFORMAT = "yyyy-MM-dd HH:mm:ss";
 
     public static String convertResourceOutputToString(Resource resource) throws IOException {
         return IOUtils.toString(resource.getInputStream());
     }
+
+    public static String GetUTCdatetimeAsString()
+    {
+        final SimpleDateFormat sdf = new SimpleDateFormat(DATEFORMAT);
+        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+        return  sdf.format(new Date());
+    }
+
 
 }
