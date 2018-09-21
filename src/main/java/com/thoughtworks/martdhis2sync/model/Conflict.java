f98794ff0981487173d96d81c373b715cb690e4a
Pratibhasagar V <pratibav@thoughtworks.com>
2018-09-22 02:10:30 +0530

    Pratibha | ### | Refactor TrackedEntityInstanceWriter.java and TrackedEntityInstanceWriterTest.java. Add more tests covering 409 exceptions for this class.

--- src/main/java/com/thoughtworks/martdhis2sync/model/Conflict.java
+++ src/main/java/com/thoughtworks/martdhis2sync/model/Conflict.java
@@ -13,6 +13,7 @@ public class Conflict {
     public static final String CONFLICT_VAL_ORG_UNIT = "No org unit ID in tracked entity instance object";
     public static final String CONFLICT_OBJ_TEI_TYPE = "TrackedEntityInstance.trackedEntityType";
     public static final String CONFLICT_OBJ_ATTRIBUTE = "Attribute.attribute";
+    public static final String CONFLICT_OBJ_ATTRIBUTE_VALUE = "Attribute.value";
     public static final String CONFLICT_OBJ_ENROLLMENT_DATE = "Enrollment.date";
     public static final String CONFLICT_OBJ_ENROLLMENT_INCIDENT_DATE = "Enrollment.incidentDate";
 
