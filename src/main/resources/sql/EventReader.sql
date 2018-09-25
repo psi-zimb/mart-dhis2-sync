733ddbcdae07fd6ecc87e447a899124a9c58c70c
Pratibhasagar V <pratibav@thoughtworks.com>
2018-09-25 14:44:03 +0530

    Pratibha | #64 | Update EventReader sql to get records only later than the marker event date.

--- src/main/resources/sql/EventReader.sql
+++ src/main/resources/sql/EventReader.sql
@@ -1,8 +1,11 @@
 SELECT event.*,
-       et.instance_id,
-       et.enrollment_id,
-       ot.id as orgunit_id
-FROM %s event
-INNER JOIN orgunit_tracker ot ON event."OrgUnit" = ot.orgunit
-INNER JOIN instance_tracker it ON event."Patient_Identifier" = it.patient_id
-INNER JOIN enrollment_tracker et ON it.instance_id = et.instance_id
+  enrTracker.instance_id,
+  enrTracker.enrollment_id,
+  orgTracker.id as orgunit_id
+FROM hiv_event_view event
+INNER JOIN orgunit_tracker orgTracker ON event."OrgUnit" = orgTracker.orgunit
+INNER JOIN instance_tracker insTracker ON event."Patient_Identifier" = insTracker.patient_id
+INNER JOIN enrollment_tracker enrTracker ON insTracker.instance_id = enrTracker.instance_id
+WHERE event.date_created > COALESCE((SELECT last_synced_date
+  FROM marker
+  WHERE category='event' AND program_name='%s'), '-infinity');
