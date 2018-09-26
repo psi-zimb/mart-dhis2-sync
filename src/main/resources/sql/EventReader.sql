dbb2bf5fc12a47296805235fc373f2883b30dd82
Pratibhasagar V <pratibav@thoughtworks.com>
2018-09-26 17:15:18 +0530

    Pratibha | ### | Parameterize the event mapping table in the EventReader sql.

--- src/main/resources/sql/EventReader.sql
+++ src/main/resources/sql/EventReader.sql
@@ -2,7 +2,7 @@ SELECT event.*,
   enrTracker.instance_id,
   enrTracker.enrollment_id,
   orgTracker.id as orgunit_id
-FROM hiv_event_view event
+FROM %s event
 INNER JOIN orgunit_tracker orgTracker ON event."OrgUnit" = orgTracker.orgunit
 INNER JOIN instance_tracker insTracker ON event."Patient_Identifier" = insTracker.patient_id
 INNER JOIN enrollment_tracker enrTracker ON insTracker.instance_id = enrTracker.instance_id
