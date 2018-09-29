cc9721ba57565456923d63bc88a069b00a48dea4
Pratibhasagar V <pratibav@thoughtworks.com>
2018-09-29 18:13:43 +0530

    Pratibha | #72 | Move the requestParams to requestBody. Receive comment as a part of DHISSyncRequestBody.

--- src/main/java/com/thoughtworks/martdhis2sync/controller/PushController.java
+++ src/main/java/com/thoughtworks/martdhis2sync/controller/PushController.java
@@ -1,16 +1,13 @@
 package com.thoughtworks.martdhis2sync.controller;
 
 import com.google.gson.Gson;
+import com.thoughtworks.martdhis2sync.model.DHISSyncRequestBody;
 import com.thoughtworks.martdhis2sync.model.LookupTable;
 import com.thoughtworks.martdhis2sync.model.MappingJson;
-import com.thoughtworks.martdhis2sync.service.EventService;
-import com.thoughtworks.martdhis2sync.service.LoggerService;
-import com.thoughtworks.martdhis2sync.service.MappingService;
-import com.thoughtworks.martdhis2sync.service.ProgramEnrollmentService;
-import com.thoughtworks.martdhis2sync.service.TEIService;
+import com.thoughtworks.martdhis2sync.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.SyncFailedException;
@@ -43,29 +40,32 @@ public class PushController {
     private static final int COMMA_AND_SPACE_SIZE = 2;
 
     @PutMapping(value = "/pushData")
-    public void pushData(@RequestParam String service, @RequestParam String user)
+    public void pushData(@RequestBody DHISSyncRequestBody requestBody)
             throws Exception {
         IS_DELTA_EXISTS = false;
         failedReason = new StringBuilder();
-        loggerService.addLog(service, user, "Comments");
+        loggerService.addLog(requestBody.getService(), requestBody.getUser(), requestBody.getComment());
 
-        Map<String, Object> mapping = mappingService.getMapping(service);
+        Map<String, Object> mapping = mappingService.getMapping(requestBody.getService());
 
         Gson gson = new Gson();
         LookupTable lookupTable = gson.fromJson(mapping.get("lookup_table").toString(), LookupTable.class);
         MappingJson mappingJson = gson.fromJson(mapping.get("mapping_json").toString(), MappingJson.class);
 
         try {
-            teiService.triggerJob(service, user, lookupTable.getInstance(), mappingJson.getInstance());
-            programEnrollmentService.triggerJob(service, user, lookupTable.getEnrollments());
-            eventService.triggerJob(service, user, lookupTable.getEvent(), mappingJson.getEvent(), lookupTable.getEnrollments());
-            if(!IS_DELTA_EXISTS) {
-                loggerService.updateLog(service, FAILED, "No data to sync");
+            teiService.triggerJob(requestBody.getService(), requestBody.getUser(),
+                    lookupTable.getInstance(), mappingJson.getInstance());
+            programEnrollmentService.triggerJob(requestBody.getService(),
+                    requestBody.getUser(), lookupTable.getEnrollments());
+            eventService.triggerJob(requestBody.getService(), requestBody.getUser(),
+                    lookupTable.getEvent(), mappingJson.getEvent(), lookupTable.getEnrollments());
+            if (!IS_DELTA_EXISTS) {
+                loggerService.updateLog(requestBody.getService(), FAILED, "No data to sync");
                 throw new Exception("NO DATA TO SYNC");
             }
-            loggerService.updateLog(service, SUCCESS, "");
+            loggerService.updateLog(requestBody.getService(), SUCCESS, "");
         } catch (SyncFailedException e) {
-            loggerService.updateLog(service, FAILED, removeChars(failedReason, COMMA_AND_SPACE_SIZE));
+            loggerService.updateLog(requestBody.getService(), FAILED, removeChars(failedReason, COMMA_AND_SPACE_SIZE));
         }
     }
 }
