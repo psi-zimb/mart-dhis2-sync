c8d216c9ccea710686ecca3ee58296673d0522c3
Mahitha Arveti <mahitha.arveti@gmail.com>
2018-09-29 12:57:41 +0530

    Mahitha | #70 | Insert a row into log table with status pending on sync

---
+++ src/test/java/com/thoughtworks/martdhis2sync/service/LoggerServiceTest.java
@@ -0,0 +1,40 @@
+package com.thoughtworks.martdhis2sync.service;
+
+import com.thoughtworks.martdhis2sync.CommonTestHelper;
+import com.thoughtworks.martdhis2sync.dao.LoggerDAO;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.powermock.api.mockito.PowerMockito.doNothing;
+
+@RunWith(PowerMockRunner.class)
+public class LoggerServiceTest {
+
+    @Mock
+    private LoggerDAO loggerDAO;
+
+    private LoggerService loggerService;
+
+    @Before
+    public void setUp() throws Exception {
+        loggerService = new LoggerService();
+        CommonTestHelper.setValuesForMemberFields(loggerService, "loggerDAO", loggerDAO);
+    }
+
+    @Test
+    public void shouldCallLoggerDAO() {
+        String service = "HT Service";
+        String user = "superman";
+        String comments = "comments";
+        doNothing().when(loggerDAO).addLog(service, user, comments);
+
+        loggerService.addLog(service, user, comments);
+
+        verify(loggerDAO, times(1)).addLog(service, user, comments);
+    }
+}
