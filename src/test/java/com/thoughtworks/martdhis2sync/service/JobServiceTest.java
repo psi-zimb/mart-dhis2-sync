07590aa4ccd531209bd957b72aa5643de9f3aae3
Mahitha Arveti <mahitha.arveti@gmail.com>
2018-10-07 17:50:27 +0530

    Mahitha | #70 | Update failure reason for batch exceptions

--- src/test/java/com/thoughtworks/martdhis2sync/service/JobServiceTest.java
+++ src/test/java/com/thoughtworks/martdhis2sync/service/JobServiceTest.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.martdhis2sync.service;
 
+import com.thoughtworks.martdhis2sync.controller.PushController;
 import com.thoughtworks.martdhis2sync.listener.JobCompletionNotificationListener;
 import com.thoughtworks.martdhis2sync.step.TrackedEntityInstanceStep;
 import org.junit.Before;
@@ -9,7 +10,12 @@ import org.mockito.Mock;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-import org.springframework.batch.core.*;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.job.builder.FlowJobBuilder;
 import org.springframework.batch.core.job.builder.JobBuilder;
@@ -19,9 +25,11 @@ import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 
 import java.io.SyncFailedException;
+import java.util.Collections;
 import java.util.Date;
 
 import static com.thoughtworks.martdhis2sync.CommonTestHelper.setValuesForMemberFields;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.times;
@@ -74,6 +82,9 @@ public class JobServiceTest {
     @Mock
     private JobExecution execution;
 
+    @Mock
+    private Throwable throwable;
+
     private JobService jobService;
 
     private String lookUpTable = "patient_identifier";
@@ -88,6 +99,7 @@ public class JobServiceTest {
         setValuesForMemberFields(jobService, "jobLauncher", jobLauncher);
         setValuesForMemberFields(jobService, "listener", listener);
         setValuesForMemberFields(jobService, "jobBuilderFactory", jobBuilderFactory);
+        PushController.failedReason = new StringBuilder();
     }
 
     @Test
@@ -150,6 +162,42 @@ public class JobServiceTest {
         verify(parametersBuilder, times(1)).toJobParameters();
         verify(jobLauncher, times(1)).run(job, jobParameters);
         verify(execution, times(1)).getStatus();
+    }
+
+    @Test
+    public void shouldUpdateFailedReasonWhenExceptionIsThere() throws Exception {
+        jobMocks();
+        jobParametersMocks();
+        String expMessage = "Failed to Initialize the Reader";
+
+        when(jobLauncher.run(job, jobParameters)).thenReturn(execution);
+        when(execution.getStatus()).thenReturn(BatchStatus.FAILED);
+        when(execution.getAllFailureExceptions()).thenReturn(Collections.singletonList(throwable));
+        when(throwable.getMessage()).thenReturn(expMessage);
+
+        try {
+            jobService.triggerJob(programName, user, lookUpTable, jobName, instanceStep, mappingObj);
+        } catch (SyncFailedException e) {
+            verify(jobBuilderFactory, times(1)).get("syncTrackedEntityInstance");
+            verifyNew(RunIdIncrementer.class, times(1)).withNoArguments();
+            verify(jobBuilder, times(1)).incrementer(runIdIncrementer);
+            verify(jobBuilder, times(1)).listener(listener);
+            verify(instanceStep, times(1)).get(lookUpTable, programName, mappingObj);
+            verify(jobBuilder, times(1)).flow(step);
+            verify(flowBuilder, times(1)).end();
+            verify(flowJobBuilder, times(1)).build();
+
+            verifyNew(JobParametersBuilder.class, times(1)).withNoArguments();
+            verify(parametersBuilder, times(1)).addDate(anyString(), any(Date.class));
+            verify(parametersBuilder, times(1)).addString("service", programName);
+            verify(parametersBuilder, times(1)).toJobParameters();
+            verify(jobLauncher, times(1)).run(job, jobParameters);
+            verify(execution, times(1)).getStatus();
+            verify(execution, times(1)).getAllFailureExceptions();
+            verify(throwable, times(1)).getMessage();
+
+            assertEquals(expMessage + ", ", PushController.failedReason.toString());
+        }
     }
 
     private void jobMocks() throws Exception {
