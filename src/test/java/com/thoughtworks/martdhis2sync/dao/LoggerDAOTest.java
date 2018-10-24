8ba59e4eebf2bfb054f7b22a979058076e1dee69
Mahitha Arveti <mahitha.arveti@gmail.com>
2018-10-24 15:14:11 +0530

    Mahitha | #000 | Removed class from prepareForTest annotationin the same class tests

--- src/test/java/com/thoughtworks/martdhis2sync/dao/LoggerDAOTest.java
+++ src/test/java/com/thoughtworks/martdhis2sync/dao/LoggerDAOTest.java
@@ -16,23 +16,21 @@ import java.util.Date;
 
 import static com.thoughtworks.martdhis2sync.CommonTestHelper.setValuesForMemberFields;
 import static com.thoughtworks.martdhis2sync.util.BatchUtil.DATEFORMAT_WITH_24HR_TIME;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.verifyStatic;
 import static org.powermock.api.mockito.PowerMockito.when;
-import static org.powermock.api.mockito.PowerMockito.whenNew;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({MapSqlParameterSource.class, BatchUtil.class, LoggerDAO.class})
+@PrepareForTest({BatchUtil.class})
 @PowerMockIgnore("javax.management.*")
 public class LoggerDAOTest {
 
     @Mock
     private NamedParameterJdbcTemplate parameterJdbcTemplate;
-
-    @Mock
-    private MapSqlParameterSource mapSqlParameterSource;
 
     @Mock
     private Logger logger;
@@ -45,11 +43,7 @@ public class LoggerDAOTest {
     private String service = "HT Service";
     private String user = "Superman";
     private String comments = "comments";
-    String sql = "INSERT INTO log (program, synced_by, comments, status, status_info, date_created) " +
-            "VALUES (:service, :user, :comments, 'pending', '', :dateCreated);";
-    String updateSql = "UPDATE log SET status = :status, status_info = :statusInfo " +
-            "WHERE program = :service AND status = 'pending';";
-    String dateStr = "2018-01-02 10:00:00";
+    private String dateStr = "2018-01-02 10:00:00";
 
     @Before
     public void setUp() throws Exception {
@@ -57,37 +51,35 @@ public class LoggerDAOTest {
         setValuesForMemberFields(loggerDAO, "parameterJdbcTemplate", parameterJdbcTemplate);
         setValuesForMemberFields(loggerDAO, "logger", logger);
         mockStatic(BatchUtil.class);
-        whenNew(MapSqlParameterSource.class).withNoArguments().thenReturn(mapSqlParameterSource);
-        whenNew(Date.class).withNoArguments().thenReturn(date);
-        when(BatchUtil.getStringFromDate(date, DATEFORMAT_WITH_24HR_TIME)).thenReturn(dateStr);
+        when(BatchUtil.getStringFromDate(any(Date.class), anyString())).thenReturn(dateStr);
         when(BatchUtil.getDateFromString(dateStr, DATEFORMAT_WITH_24HR_TIME)).thenReturn(date);
     }
 
     @Test
     public void shouldLogSuccessMessageOnLogInsert() {
-        when(parameterJdbcTemplate.update(sql, mapSqlParameterSource)).thenReturn(1);
+        when(parameterJdbcTemplate.update(anyString(), any(MapSqlParameterSource.class))).thenReturn(1);
 
         loggerDAO.addLog(service, user, comments);
 
         verifyStatic();
-        BatchUtil.getStringFromDate(date, DATEFORMAT_WITH_24HR_TIME);
+        BatchUtil.getStringFromDate(any(Date.class), anyString());
         verifyStatic();
         BatchUtil.getDateFromString(dateStr, BatchUtil.DATEFORMAT_WITH_24HR_TIME);
-        verify(parameterJdbcTemplate, times(1)).update(sql, mapSqlParameterSource);
+        verify(parameterJdbcTemplate, times(1)).update(anyString(), any(MapSqlParameterSource.class));
         verify(logger, times(1)).info("LoggerDAO: Successfully inserted into log table");
     }
 
     @Test
     public void shouldLogErrorMessageOnLogInsertFail() {
-        when(parameterJdbcTemplate.update(sql, mapSqlParameterSource)).thenReturn(0);
+        when(parameterJdbcTemplate.update(anyString(), any(MapSqlParameterSource.class))).thenReturn(0);
 
         loggerDAO.addLog(service, user, comments);
 
         verifyStatic();
-        BatchUtil.getStringFromDate(date, DATEFORMAT_WITH_24HR_TIME);
+        BatchUtil.getStringFromDate(any(Date.class), anyString());
         verifyStatic();
         BatchUtil.getDateFromString(dateStr, BatchUtil.DATEFORMAT_WITH_24HR_TIME);
-        verify(parameterJdbcTemplate, times(1)).update(sql, mapSqlParameterSource);
+        verify(parameterJdbcTemplate, times(1)).update(anyString(), any(MapSqlParameterSource.class));
         verify(logger, times(1)).error("LoggerDAO: Failed to insert into log table");
     }
 
@@ -96,11 +88,11 @@ public class LoggerDAOTest {
         String status = "success";
         String statusInfo = "";
 
-        when(parameterJdbcTemplate.update(updateSql, mapSqlParameterSource)).thenReturn(1);
+        when(parameterJdbcTemplate.update(anyString(), any(MapSqlParameterSource.class))).thenReturn(1);
 
         loggerDAO.updateLog(service, status, statusInfo);
 
-        verify(parameterJdbcTemplate, times(1)).update(updateSql, mapSqlParameterSource);
+        verify(parameterJdbcTemplate, times(1)).update(anyString(), any(MapSqlParameterSource.class));
         verify(logger, times(1)).info("LoggerDAO: Successfully updated status of the HT Service sync");
     }
 
@@ -109,11 +101,11 @@ public class LoggerDAOTest {
         String status = "failed";
         String statusInfo = "conflict";
 
-        when(parameterJdbcTemplate.update(updateSql, mapSqlParameterSource)).thenReturn(0);
+        when(parameterJdbcTemplate.update(anyString(), any(MapSqlParameterSource.class))).thenReturn(0);
 
         loggerDAO.updateLog(service, status, statusInfo);
 
-        verify(parameterJdbcTemplate, times(1)).update(updateSql, mapSqlParameterSource);
+        verify(parameterJdbcTemplate, times(1)).update(anyString(), any(MapSqlParameterSource.class));
         verify(logger, times(1)).error("LoggerDAO: Failed updated status of the HT Service sync");
     }
 }
