d01c6b9a34fea458eebf849a0704975ed302f905
Mahitha Arveti <mahitha.arveti@gmail.com>
2018-10-24 15:14:11 +0530

    Mahitha | #000 | Removed class from prepareForTest annotationin the same class tests

--- src/test/java/com/thoughtworks/martdhis2sync/service/JobServiceTest.java
+++ src/test/java/com/thoughtworks/martdhis2sync/service/JobServiceTest.java
@@ -7,38 +7,29 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.job.builder.FlowJobBuilder;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.job.builder.JobFlowBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 
 import java.io.SyncFailedException;
 import java.util.Collections;
-import java.util.Date;
 
 import static com.thoughtworks.martdhis2sync.CommonTestHelper.setValuesForMemberFields;
-import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.powermock.api.mockito.PowerMockito.verifyNew;
 import static org.powermock.api.mockito.PowerMockito.when;
-import static org.powermock.api.mockito.PowerMockito.whenNew;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(JobService.class)
 @PowerMockIgnore("javax.management.*")
 public class JobServiceTest {
 
@@ -50,9 +41,6 @@ public class JobServiceTest {
 
     @Mock
     private JobBuilder jobBuilder;
-
-    @Mock
-    private RunIdIncrementer runIdIncrementer;
 
     @Mock
     private JobCompletionNotificationListener listener;
@@ -71,12 +59,6 @@ public class JobServiceTest {
 
     @Mock
     private Job job;
-
-    @Mock
-    private JobParametersBuilder parametersBuilder;
-
-    @Mock
-    private JobParameters jobParameters;
 
     @Mock
     private JobExecution execution;
@@ -108,33 +90,24 @@ public class JobServiceTest {
     public void shouldTriggerTheJob() throws Exception {
 
         jobMocks();
-        jobParametersMocks();
-        when(jobLauncher.run(job, jobParameters)).thenReturn(execution);
+        when(jobLauncher.run(any(Job.class), any(JobParameters.class))).thenReturn(execution);
 
         jobService.triggerJob(programName, user, lookUpTable, jobName, instanceStep, mappingObj);
 
         verify(jobBuilderFactory, times(1)).get("syncTrackedEntityInstance");
-        verifyNew(RunIdIncrementer.class, times(1)).withNoArguments();
-        verify(jobBuilder, times(1)).incrementer(runIdIncrementer);
+        verify(jobBuilder, times(1)).incrementer(any());
         verify(jobBuilder, times(1)).listener(listener);
         verify(instanceStep, times(1)).get(lookUpTable, programName, mappingObj);
         verify(jobBuilder, times(1)).flow(step);
         verify(flowBuilder, times(1)).end();
         verify(flowJobBuilder, times(1)).build();
-
-        verifyNew(JobParametersBuilder.class, times(1)).withNoArguments();
-        verify(parametersBuilder, times(1)).addDate(anyString(), any(Date.class));
-        verify(parametersBuilder, times(1)).addString("service", programName);
-        verify(parametersBuilder, times(1)).toJobParameters();
-        verify(jobLauncher, times(1)).run(job, jobParameters);
     }
 
     @Test(expected = JobExecutionAlreadyRunningException.class)
     public void shouldThrowJobExecutionAlreadyRunningException() throws Exception {
 
         jobMocks();
-        jobParametersMocks();
-        when(jobLauncher.run(job, jobParameters))
+        when(jobLauncher.run(any(Job.class), any(JobParameters.class)))
                 .thenThrow(new JobExecutionAlreadyRunningException("Job Execution Already Running"));
 
         jobService.triggerJob(programName, user, lookUpTable, jobName, instanceStep, mappingObj);
@@ -143,36 +116,26 @@ public class JobServiceTest {
     @Test(expected = SyncFailedException.class)
     public void shouldThrowSyncFailedException() throws Exception {
         jobMocks();
-        jobParametersMocks();
-        when(jobLauncher.run(job, jobParameters)).thenReturn(execution);
+        when(jobLauncher.run(any(Job.class), any(JobParameters.class))).thenReturn(execution);
         when(execution.getStatus()).thenReturn(BatchStatus.FAILED);
 
         jobService.triggerJob(programName, user, lookUpTable, jobName, instanceStep, mappingObj);
 
         verify(jobBuilderFactory, times(1)).get("syncTrackedEntityInstance");
-        verifyNew(RunIdIncrementer.class, times(1)).withNoArguments();
-        verify(jobBuilder, times(1)).incrementer(runIdIncrementer);
+        verify(jobBuilder, times(1)).incrementer(any());
         verify(jobBuilder, times(1)).listener(listener);
         verify(instanceStep, times(1)).get(lookUpTable, programName, mappingObj);
         verify(jobBuilder, times(1)).flow(step);
         verify(flowBuilder, times(1)).end();
         verify(flowJobBuilder, times(1)).build();
-
-        verifyNew(JobParametersBuilder.class, times(1)).withNoArguments();
-        verify(parametersBuilder, times(1)).addDate(anyString(), any(Date.class));
-        verify(parametersBuilder, times(1)).addString("service", programName);
-        verify(parametersBuilder, times(1)).toJobParameters();
-        verify(jobLauncher, times(1)).run(job, jobParameters);
-        verify(execution, times(1)).getStatus();
     }
 
     @Test
     public void shouldUpdateStatusInfoWhenExceptionIsThere() throws Exception {
         jobMocks();
-        jobParametersMocks();
         String expMessage = "Failed to Initialize the Reader";
 
-        when(jobLauncher.run(job, jobParameters)).thenReturn(execution);
+        when(jobLauncher.run(any(Job.class), any(JobParameters.class))).thenReturn(execution);
         when(execution.getStatus()).thenReturn(BatchStatus.FAILED);
         when(execution.getAllFailureExceptions()).thenReturn(Collections.singletonList(throwable));
         when(throwable.getMessage()).thenReturn(expMessage);
@@ -181,19 +144,14 @@ public class JobServiceTest {
             jobService.triggerJob(programName, user, lookUpTable, jobName, instanceStep, mappingObj);
         } catch (SyncFailedException e) {
             verify(jobBuilderFactory, times(1)).get("syncTrackedEntityInstance");
-            verifyNew(RunIdIncrementer.class, times(1)).withNoArguments();
-            verify(jobBuilder, times(1)).incrementer(runIdIncrementer);
+            verify(jobBuilder, times(1)).incrementer(any());
             verify(jobBuilder, times(1)).listener(listener);
             verify(instanceStep, times(1)).get(lookUpTable, programName, mappingObj);
             verify(jobBuilder, times(1)).flow(step);
             verify(flowBuilder, times(1)).end();
             verify(flowJobBuilder, times(1)).build();
 
-            verifyNew(JobParametersBuilder.class, times(1)).withNoArguments();
-            verify(parametersBuilder, times(1)).addDate(anyString(), any(Date.class));
-            verify(parametersBuilder, times(1)).addString("service", programName);
-            verify(parametersBuilder, times(1)).toJobParameters();
-            verify(jobLauncher, times(1)).run(job, jobParameters);
+            verify(jobLauncher, times(1)).run(any(Job.class), any(JobParameters.class));
             verify(execution, times(1)).getStatus();
             verify(execution, times(1)).getAllFailureExceptions();
             verify(throwable, times(1)).getMessage();
@@ -204,20 +162,11 @@ public class JobServiceTest {
 
     private void jobMocks() throws Exception {
         when(jobBuilderFactory.get(jobName)).thenReturn(jobBuilder);
-        whenNew(RunIdIncrementer.class).withNoArguments().thenReturn(runIdIncrementer);
-        when(jobBuilder.incrementer(runIdIncrementer)).thenReturn(jobBuilder);
+        when(jobBuilder.incrementer(any())).thenReturn(jobBuilder);
         when(jobBuilder.listener(listener)).thenReturn(jobBuilder);
         when(instanceStep.get(lookUpTable, programName, mappingObj)).thenReturn(step);
         when(jobBuilder.flow(step)).thenReturn(flowBuilder);
         when(flowBuilder.end()).thenReturn(flowJobBuilder);
         when(flowJobBuilder.build()).thenReturn(job);
-    }
-
-    private void jobParametersMocks() throws Exception {
-        whenNew(JobParametersBuilder.class).withNoArguments().thenReturn(parametersBuilder);
-        when(parametersBuilder.addDate(anyString(), any(Date.class))).thenReturn(parametersBuilder);
-        when(parametersBuilder.addString("service", programName)).thenReturn(parametersBuilder);
-        when(parametersBuilder.addString("user", user)).thenReturn(parametersBuilder);
-        when(parametersBuilder.toJobParameters()).thenReturn(jobParameters);
     }
 }
