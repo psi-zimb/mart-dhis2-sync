90709bce4d03c6918d515a4b23b1ffc8b72b5f3c
Pratibhasagar V <pratibav@thoughtworks.com>
2018-11-09 18:37:18 +0530

    Pratibha, Pavan | ### | Add @PostConstruct annotation to OrgUnitService. Move @EnableScheduling annotation to MartDhis2SyncApplication.java.

--- src/main/java/com/thoughtworks/martdhis2sync/MartDhis2SyncApplication.java
+++ src/main/java/com/thoughtworks/martdhis2sync/MartDhis2SyncApplication.java
@@ -3,9 +3,11 @@ package com.thoughtworks.martdhis2sync;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableBatchProcessing
+@EnableScheduling
 public class MartDhis2SyncApplication {
 
     public static void main(String[] args) {
