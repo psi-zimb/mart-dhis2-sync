f8103a3c347d45bda6d2316185a532db79f85579
Mahitha Arveti <mahitha.arveti@gmail.com>
2018-12-01 19:06:32 +0530

    Mahitha | #142 | Processing the enrollment response

--- src/main/java/com/thoughtworks/martdhis2sync/util/EnrollmentUtil.java
+++ src/main/java/com/thoughtworks/martdhis2sync/util/EnrollmentUtil.java
@@ -1,9 +1,13 @@
 package com.thoughtworks.martdhis2sync.util;
 
+import com.thoughtworks.martdhis2sync.model.EnrollmentAPIPayLoad;
+
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class EnrollmentUtil {
 
     public static Date date = new Date(Long.MIN_VALUE);
-
+    public static List<EnrollmentAPIPayLoad> enrollmentsToSaveInTracker = new ArrayList<>();
 }
