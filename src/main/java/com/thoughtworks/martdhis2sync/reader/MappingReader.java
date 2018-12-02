f98010f715fbbfb740c4d5820880eb1b30643244
Mahitha Arveti <mahitha.arveti@gmail.com>
2018-12-02 20:38:53 +0530

    Mahitha | #142 | Added reader to get only new completed enrollments

--- src/main/java/com/thoughtworks/martdhis2sync/reader/MappingReader.java
+++ src/main/java/com/thoughtworks/martdhis2sync/reader/MappingReader.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.martdhis2sync.reader;
 
 import com.thoughtworks.martdhis2sync.util.BatchUtil;
+import com.thoughtworks.martdhis2sync.util.EnrollmentUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.database.JdbcCursorItemReader;
@@ -13,6 +14,9 @@ import org.springframework.stereotype.Component;
 import javax.sql.DataSource;
 import java.io.IOException;
 import java.util.Map;
+
+import static com.thoughtworks.martdhis2sync.util.BatchUtil.DATEFORMAT_WITH_24HR_TIME;
+import static com.thoughtworks.martdhis2sync.util.BatchUtil.getStringFromDate;
 
 @Component
 public class MappingReader {
@@ -31,6 +35,9 @@ public class MappingReader {
 
     @Value("classpath:sql/NewCompletedEnrollmentWithEvents.sql")
     private Resource newCompletedEnrWithEventsResource;
+
+    @Value("classpath:sql/NewCompletedEnrollment.sql")
+    private Resource newCompletedEnrResource;
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -70,6 +77,12 @@ public class MappingReader {
 
     public JdbcCursorItemReader<Map<String, Object>> getNewCompletedEnrollmentWithEventsReader(String eventLookupTable, String programName, String enrollmentLookupTable) {
         String sql = String.format(getSql(newCompletedEnrWithEventsResource), enrollmentLookupTable, eventLookupTable, programName);
+        return get(sql);
+    }
+
+    public JdbcCursorItemReader<Map<String, Object>> getNewCompletedEnrollmentReader(String enrollmentLookupTable, String programName) {
+        String sql = String.format(getSql(newCompletedEnrResource), enrollmentLookupTable, programName,
+                getStringFromDate(EnrollmentUtil.date, DATEFORMAT_WITH_24HR_TIME));
         return get(sql);
     }
 }
