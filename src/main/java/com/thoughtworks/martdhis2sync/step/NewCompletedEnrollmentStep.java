e8d12af6ab809644d1d2c02d650ff766a24e7051
Mahitha Arveti <mahitha.arveti@gmail.com>
2018-12-03 18:17:26 +0530

    Mahitha | #142 | Added tasklet step to send enrollments to DHIS

---
+++ src/main/java/com/thoughtworks/martdhis2sync/step/NewCompletedEnrollmentStep.java
@@ -0,0 +1,22 @@
+package com.thoughtworks.martdhis2sync.step;
+
+import com.thoughtworks.martdhis2sync.writer.NewCompletedEnrollmentTasklet;
+import org.springframework.batch.core.Step;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NewCompletedEnrollmentStep {
+
+    @Autowired
+    private StepFactory stepFactory;
+
+    @Autowired
+    private NewCompletedEnrollmentTasklet tasklet;
+
+    private static final String NCE_STEP_NAME = "New Completed Enrollment Step:: ";
+
+    public Step get() {
+        return stepFactory.build(NCE_STEP_NAME, tasklet);
+    }
+}
