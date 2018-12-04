5e6cecff3dfe1ab6fbae529894be9e805a176a25
Pratibhasagar V <pratibav@thoughtworks.com>
2018-12-04 14:49:43 +0530

    Pratibha | #142 | Move updateLatestEventDateCreated to EventUtil and updateLatestEnrollmentDateCreated to EnrollmentUtil class.

--- src/main/java/com/thoughtworks/martdhis2sync/util/EnrollmentUtil.java
+++ src/main/java/com/thoughtworks/martdhis2sync/util/EnrollmentUtil.java
@@ -6,8 +6,18 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import static com.thoughtworks.martdhis2sync.util.BatchUtil.DATEFORMAT_WITH_24HR_TIME;
+import static com.thoughtworks.martdhis2sync.util.BatchUtil.getDateFromString;
+
 public class EnrollmentUtil {
 
     public static Date date = new Date(Long.MIN_VALUE);
     public static List<EnrollmentAPIPayLoad> enrollmentsToSaveInTracker = new ArrayList<>();
+
+    public static void updateLatestEnrollmentDateCreated(String dateCreated) {
+        Date bahmniDateCreated = getDateFromString(dateCreated, DATEFORMAT_WITH_24HR_TIME);
+        if (date.compareTo(bahmniDateCreated) < 1) {
+            date = bahmniDateCreated;
+        }
+    }
 }
