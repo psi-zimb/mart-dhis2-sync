fb1a32072361ded379ac800e576735f0a78d046f
Pratibhasagar V <pratibav@thoughtworks.com>
2018-12-04 14:52:26 +0530

    Pratibha, Mahitha | #142 | Add UpdatedCompletedEnrollmentWithEvents sql file. Update MappingReader to load this sql file.

--- src/main/java/com/thoughtworks/martdhis2sync/model/EnrollmentAPIPayLoad.java
+++ src/main/java/com/thoughtworks/martdhis2sync/model/EnrollmentAPIPayLoad.java
@@ -2,11 +2,13 @@ package com.thoughtworks.martdhis2sync.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class EnrollmentAPIPayLoad {
     public static final String STATUS_ACTIVE = "ACTIVE";
     public static final String STATUS_COMPLETED = "COMPLETED";
