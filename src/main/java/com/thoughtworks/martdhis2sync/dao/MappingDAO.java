5d62ab8def37cf00265703bd4c922e73a0235ace
Pavan <pavankt@thoughtworks.com>
2018-12-04 15:30:21 +0530

    Pavan | #135 | Added method in MappingDAO to get searchable attributes

--- src/main/java/com/thoughtworks/martdhis2sync/dao/MappingDAO.java
+++ src/main/java/com/thoughtworks/martdhis2sync/dao/MappingDAO.java
@@ -1,10 +1,18 @@
 package com.thoughtworks.martdhis2sync.dao;
 
+import com.google.gson.Gson;
+import com.thoughtworks.martdhis2sync.model.Config;
+import com.thoughtworks.martdhis2sync.model.LookupTable;
+import com.thoughtworks.martdhis2sync.util.BatchUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 @Component
@@ -14,11 +22,34 @@ public class MappingDAO {
     @Qualifier("jdbcTemplate")
     private JdbcTemplate jdbcTemplate;
 
+    @Value("classpath:sql/Searchable.sql")
+    private Resource searchableResource;
 
     public Map<String, Object> getMapping(String mapping) {
-        String sql = String.format("SELECT lookup_table, mapping_json FROM mapping WHERE mapping_name='%s'", mapping);
+        String sql = String.format("SELECT lookup_table, mapping_json, config FROM mapping WHERE mapping_name='%s'", mapping);
 
         return jdbcTemplate.queryForMap(sql);
     }
 
+    public List<Map<String, Object>> getSearchableFields(String mappingName) throws IOException {
+        StringBuilder columns = new StringBuilder();
+        Gson gson = new Gson();
+        Map<String, Object> mapping = getMapping(mappingName);
+
+        gson.fromJson(mapping.get("config").toString(), Config.class).getSearchable().forEach(column -> {
+            columns.append("\"");
+            columns.append(column);
+            columns.append("\"");
+            columns.append(",");
+        });
+
+        return jdbcTemplate.queryForList(
+                String.format(
+                        BatchUtil.convertResourceOutputToString(searchableResource),
+                        columns.substring(0, columns.length() - 1),
+                        gson.fromJson(mapping.get("lookup_table").toString(), LookupTable.class).getInstance(),
+                        mappingName
+                )
+        );
+    }
 }
---
+++ src/main/resources/sql/Searchable.sql
@@ -0,0 +1,1 @@
+SELECT %s FROM %s pi INNER JOIN marker m ON pi.date_created :: TIMESTAMP > COALESCE(m.last_synced_date, '-infinity') AND category = 'instance' AND program_name = '%s';
