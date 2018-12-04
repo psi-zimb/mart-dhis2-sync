4c31dcb5dd1f0f67ab3f115e2e27c652c57b211d
Pratibhasagar V <pratibav@thoughtworks.com>
2018-12-04 17:36:30 +0530

    Mahitha, Pratibha | #142 | Added procesor for only update enrollments and events

---
+++ src/main/java/com/thoughtworks/martdhis2sync/processor/EnrollmentWithEventProcessor.java
@@ -0,0 +1,48 @@
+package com.thoughtworks.martdhis2sync.processor;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.thoughtworks.martdhis2sync.model.EnrollmentAPIPayLoad;
+import com.thoughtworks.martdhis2sync.model.Event;
+import com.thoughtworks.martdhis2sync.model.ProcessedTableRow;
+import org.springframework.stereotype.Component;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static com.thoughtworks.martdhis2sync.util.BatchUtil.DATEFORMAT_WITH_24HR_TIME;
+import static com.thoughtworks.martdhis2sync.util.EnrollmentUtil.updateLatestEnrollmentDateCreated;
+import static com.thoughtworks.martdhis2sync.util.EventUtil.updateLatestEventDateCreated;
+
+@Component
+public abstract class EnrollmentWithEventProcessor {
+
+    public ProcessedTableRow process(Object tableRow, Object mappingObj) {
+        Gson gson = new GsonBuilder().setDateFormat(DATEFORMAT_WITH_24HR_TIME).create();
+        JsonElement tableRowJsonElement = gson.toJsonTree(tableRow);
+        JsonElement mappingObjJsonElement = gson.toJsonTree(mappingObj);
+
+        JsonObject tableRowJsonObject = tableRowJsonElement.getAsJsonObject();
+        JsonObject mappingJsonObject = mappingObjJsonElement.getAsJsonObject();
+
+        updateLatestEventDateCreated(tableRowJsonObject.get("date_created").getAsString());
+        updateLatestEnrollmentDateCreated(tableRowJsonObject.get("enrollment_date_created").getAsString());
+
+        Event event = getEvent(tableRowJsonObject, mappingJsonObject);
+        List<Event> events = new LinkedList<>();
+        if (event != null) {
+            events.add(event);
+        }
+        EnrollmentAPIPayLoad enrollmentAPIPayLoad = getEnrollmentAPIPayLoad(tableRowJsonObject, events);
+
+        return new ProcessedTableRow(
+                tableRowJsonObject.get("enrolled_patient_identifier").getAsString(),
+                enrollmentAPIPayLoad
+        );
+    }
+
+    abstract Event getEvent(JsonObject tableRow, JsonObject mapping);
+    abstract EnrollmentAPIPayLoad getEnrollmentAPIPayLoad(JsonObject tableRowJsonObject, List<Event> events);
+}
