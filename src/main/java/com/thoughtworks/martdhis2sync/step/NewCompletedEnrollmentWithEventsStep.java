78baa4a4aa017a21d4d2c77948ea3b414b774f83
Mahitha Arveti <mahitha.arveti@gmail.com>
2018-12-05 08:59:23 +0530

    Mahitha | #142 | Getting last synced date for both enrollments and events before starting the job

--- src/main/java/com/thoughtworks/martdhis2sync/step/NewCompletedEnrollmentWithEventsStep.java
+++ src/main/java/com/thoughtworks/martdhis2sync/step/NewCompletedEnrollmentWithEventsStep.java
@@ -2,17 +2,11 @@ package com.thoughtworks.martdhis2sync.step;
 
 import com.thoughtworks.martdhis2sync.processor.NewCompletedEnrollmentWithEventsProcessor;
 import com.thoughtworks.martdhis2sync.reader.MappingReader;
-import com.thoughtworks.martdhis2sync.util.EnrollmentUtil;
-import com.thoughtworks.martdhis2sync.util.EventUtil;
-import com.thoughtworks.martdhis2sync.util.MarkerUtil;
 import com.thoughtworks.martdhis2sync.writer.NewCompletedEnrollmentWithEventsWriter;
 import org.springframework.batch.core.Step;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import static com.thoughtworks.martdhis2sync.util.MarkerUtil.CATEGORY_ENROLLMENT;
-import static com.thoughtworks.martdhis2sync.util.MarkerUtil.CATEGORY_EVENT;
 
 @Component
 public class NewCompletedEnrollmentWithEventsStep {
@@ -29,14 +23,10 @@ public class NewCompletedEnrollmentWithEventsStep {
     @Autowired
     private StepFactory stepFactory;
 
-    @Autowired
-    private MarkerUtil markerUtil;
-
     private static final String STEP_NAME = "New Completed Enrollment With Events Step:: ";
 
     public Step get(String enrLookupTable, String envLookupTable, String programName, Object mappingObj) {
-        EnrollmentUtil.date = markerUtil.getLastSyncedDate(programName, CATEGORY_ENROLLMENT);
-        EventUtil.date = markerUtil.getLastSyncedDate(programName, CATEGORY_EVENT);
+
         return stepFactory.build(STEP_NAME,
                 mappingReader.getNewCompletedEnrollmentWithEventsReader(enrLookupTable, programName, envLookupTable),
                 getProcessor(mappingObj),
