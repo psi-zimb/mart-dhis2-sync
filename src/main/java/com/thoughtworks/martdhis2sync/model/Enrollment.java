31c69d876f40876dc1baacb86eca296ae875cbe4
Pratibhasagar V <pratibav@thoughtworks.com>
2018-12-06 11:17:23 +0530

    Pratibha, Mahitha | #142 | Use constants from EnrollmentAPIPayLoad.java instead of Enrollment.java.

--- src/main/java/com/thoughtworks/martdhis2sync/model/Enrollment.java
+++ src/main/java/com/thoughtworks/martdhis2sync/model/Enrollment.java
@@ -6,11 +6,6 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class Enrollment {
-
-    public static final String STATUS_ACTIVE = "ACTIVE";
-    public static final String STATUS_COMPLETED = "COMPLETED";
-    public static final String STATUS_CANCELLED = "CANCELLED";
-
     private String enrollment_id;
     private String instance_id;
     private String program;
