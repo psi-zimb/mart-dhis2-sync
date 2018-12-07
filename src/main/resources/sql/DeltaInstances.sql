4e7a4d7cb7044059f23992caaa930d47dc3a232d
Mahitha Arveti <mahitha.arveti@gmail.com>
2018-12-07 18:31:47 +0530

    Mahitha | #143 | Added DAO to get instanceIds of the detal patients

--- src/main/resources/sql/DeltaInstances.sql
+++ src/main/resources/sql/DeltaInstances.sql
@@ -1,5 +1,4 @@
-SELECT mappedTable.*, insTracker.instance_id, orgTracker.id as orgunit_id,
-  CASE WHEN enrTracker.enrollment_id is NULL THEN '' ELSE enrTracker.enrollment_id END AS enrollment_id
+SELECT insTracker.instance_id, mappedTable.program
 FROM %s mappedTable
 INNER JOIN instance_tracker insTracker
   ON insTracker.patient_id = mappedTable."Patient_Identifier"
@@ -9,6 +8,6 @@ LEFT JOIN enrollment_tracker enrTracker
   ON mappedTable.program = enrTracker.program
     AND enrTracker.instance_id = insTracker.instance_id
     AND enrTracker.program_unique_id = mappedTable.program_unique_id::text
-  WHERE mappedTable.date_created::TIMESTAMP > COALESCE((SELECT last_synced_date
+WHERE mappedTable.date_created::TIMESTAMP > COALESCE((SELECT last_synced_date
                                     FROM marker
                                     WHERE category='enrollment' AND program_name='%s'), '-infinity');
