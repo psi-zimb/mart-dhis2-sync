56c0dbb94abf1cc4a5e747f796e74cfd4910c6e8
Mahitha Arveti <mahitha.arveti@gmail.com>
2018-12-07 18:31:47 +0530

    Mahitha | #143 | Added DAO to get instanceIds of the detal patients

---
+++ src/main/java/com/thoughtworks/martdhis2sync/dao/PatientDAO.java
@@ -0,0 +1,34 @@
+package com.thoughtworks.martdhis2sync.dao;
+
+import com.thoughtworks.martdhis2sync.util.BatchUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class PatientDAO {
+    @Value("classpath:sql/DeltaInstances.sql")
+    private Resource deltaInstances;
+
+    @Autowired
+    @Qualifier("jdbcTemplate")
+    private JdbcTemplate jdbcTemplate;
+
+    public List<Map<String, Object>> getDeltaInstanceIds(String enrollmentTable, String programName) throws Exception {
+        String sql;
+        try {
+            sql = BatchUtil.convertResourceOutputToString(deltaInstances);
+        } catch (IOException e) {
+            throw new Exception("Error in converting sql to string:: " + e.getMessage());
+        }
+
+        return jdbcTemplate.queryForList(String.format(sql, enrollmentTable, programName));
+    }
+}
