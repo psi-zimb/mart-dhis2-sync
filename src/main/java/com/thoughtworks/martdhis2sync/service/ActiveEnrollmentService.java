10074df7fe2d78d06486292f1d2fa6040ec6af8b
Pratibhasagar V <pratibav@thoughtworks.com>
2018-12-12 11:55:01 +0530

    Pratibha | #141 | Add ActiveEnrollmentService for triggering batch jobs for active enrollments with events.

---
+++ src/main/java/com/thoughtworks/martdhis2sync/service/ActiveEnrollmentService.java
@@ -0,0 +1,66 @@
+package com.thoughtworks.martdhis2sync.service;
+
+import com.thoughtworks.martdhis2sync.step.NewActiveEnrollmentWithEventsStep;
+import com.thoughtworks.martdhis2sync.step.UpdatedActiveEnrollmentWithEventsStep;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.SyncFailedException;
+import java.util.LinkedList;
+
+@Component
+public class ActiveEnrollmentService {
+
+    @Autowired
+    private NewActiveEnrollmentWithEventsStep newEnrollmentWithEventsStep;
+
+    @Autowired
+    private UpdatedActiveEnrollmentWithEventsStep updatedEnrollmentWithEventsStep;
+
+    @Autowired
+    private JobService jobService;
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private static final String LOG_PREFIX = "Active Enrollments: ";
+    private static final String JOB_NEW_ACTIVE_ENROLLMENTS = "New Active Enrollments";
+    private static final String JOB_UPDATED_ACTIVE_ENROLLMENTS = "Updated Active Enrollments";
+
+    public void triggerJobForNewActiveEnrollments(String service, String user, String enrLookupTable,
+                                                  String evnLookupTable, Object mappingObj)
+            throws JobParametersInvalidException, JobExecutionAlreadyRunningException,
+            JobRestartException, JobInstanceAlreadyCompleteException, SyncFailedException {
+
+        LinkedList<Step> steps = new LinkedList<>();
+        steps.add(newEnrollmentWithEventsStep.get(enrLookupTable, evnLookupTable, service, mappingObj));
+        triggerJob(service, user, steps, JOB_NEW_ACTIVE_ENROLLMENTS);
+    }
+
+    public void triggerJobForUpdatedActiveEnrollments(String service, String user, String enrLookupTable,
+                                                      String evnLookupTable, Object mappingObj)
+            throws JobParametersInvalidException, JobExecutionAlreadyRunningException,
+            JobRestartException, JobInstanceAlreadyCompleteException, SyncFailedException {
+
+        LinkedList<Step> steps = new LinkedList<>();
+        steps.add(updatedEnrollmentWithEventsStep.get(enrLookupTable, evnLookupTable, service, mappingObj));
+        triggerJob(service, user, steps, JOB_UPDATED_ACTIVE_ENROLLMENTS);
+    }
+
+    private void triggerJob(String service, String user, LinkedList<Step> steps, String jobName)
+            throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException,
+            JobInstanceAlreadyCompleteException, SyncFailedException {
+        try {
+            jobService.triggerJob(service, user, jobName, steps);
+        } catch (Exception e) {
+            logger.error(LOG_PREFIX + e.getMessage());
+            throw e;
+        }
+    }
+}
---
+++ src/test/java/com/thoughtworks/martdhis2sync/service/ActiveEnrollmentServiceTest.java
@@ -0,0 +1,124 @@
+package com.thoughtworks.martdhis2sync.service;
+
+import com.thoughtworks.martdhis2sync.step.NewActiveEnrollmentWithEventsStep;
+import com.thoughtworks.martdhis2sync.step.UpdatedActiveEnrollmentWithEventsStep;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.slf4j.Logger;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+
+import java.io.SyncFailedException;
+import java.util.LinkedList;
+
+import static com.thoughtworks.martdhis2sync.CommonTestHelper.setValuesForMemberFields;
+import static org.mockito.Mockito.*;
+import static org.powermock.api.mockito.PowerMockito.doNothing;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+@RunWith(PowerMockRunner.class)
+@PowerMockIgnore("javax.management.*")
+public class ActiveEnrollmentServiceTest {
+    @Mock
+    private NewActiveEnrollmentWithEventsStep newActiveEnrollmentWithEventsStep;
+
+    @Mock
+    private UpdatedActiveEnrollmentWithEventsStep updatedActiveEnrollmentWithEventsStep;
+
+    @Mock
+    private JobService jobService;
+
+    @Mock
+    private Logger logger;
+
+    @Mock
+    private Step step;
+
+    private ActiveEnrollmentService service;
+
+    private String programName = "HT Service";
+    private String enrLookupTable = "enrollment_table";
+    private String evnLookupTable = "event_table";
+    private String user = "superman";
+    private String mappingObj = "";
+    private String jobName = "New Active Enrollments";
+    private String updateJobName = "Updated Active Enrollments";
+
+    @Before
+    public void setUp() throws Exception {
+        service = new ActiveEnrollmentService();
+
+        setValuesForMemberFields(service, "newEnrollmentWithEventsStep", newActiveEnrollmentWithEventsStep);
+        setValuesForMemberFields(service, "jobService", jobService);
+        setValuesForMemberFields(service, "logger", logger);
+        setValuesForMemberFields(service, "updatedEnrollmentWithEventsStep", updatedActiveEnrollmentWithEventsStep);
+    }
+
+    @Test
+    public void shouldTriggerTheJob() throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, SyncFailedException {
+        when(newActiveEnrollmentWithEventsStep.get(enrLookupTable, evnLookupTable, programName, mappingObj)).thenReturn(step);
+        LinkedList<Step> steps = new LinkedList<>();
+        steps.add(step);
+        doNothing().when(jobService).triggerJob(programName, user, jobName, steps);
+
+        service.triggerJobForNewActiveEnrollments(programName, user, enrLookupTable, evnLookupTable, mappingObj);
+
+        verify(jobService, times(1)).triggerJob(programName, user, jobName, steps);
+        verify(newActiveEnrollmentWithEventsStep, times(1)).get(enrLookupTable, evnLookupTable, programName, mappingObj);
+    }
+
+    @Test
+    public void shouldLogErrorOnJobFail() throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, SyncFailedException {
+        when(newActiveEnrollmentWithEventsStep.get(enrLookupTable, evnLookupTable, programName, mappingObj)).thenReturn(step);
+        LinkedList<Step> steps = new LinkedList<>();
+        steps.add(step);
+        doThrow(new JobParametersInvalidException("Invalid Params")).when(jobService).triggerJob(programName, user, jobName, steps);
+
+        try {
+            service.triggerJobForNewActiveEnrollments(programName, user, enrLookupTable, evnLookupTable, mappingObj);
+        } catch (Exception e) {
+            verify(jobService, times(1)).triggerJob(programName, user, jobName, steps);
+            verify(newActiveEnrollmentWithEventsStep, times(1)).get(enrLookupTable, evnLookupTable, programName, mappingObj);
+            verify(logger, times(1)).error("Active Enrollments: Invalid Params");
+        }
+
+    }
+
+    @Test
+    public void shouldTriggerTheJobForUpdatedActiveEnrollments() throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, SyncFailedException {
+        when(updatedActiveEnrollmentWithEventsStep.get(enrLookupTable, evnLookupTable, programName, mappingObj)).thenReturn(step);
+        LinkedList<Step> steps = new LinkedList<>();
+        steps.add(step);
+        doNothing().when(jobService).triggerJob(programName, user, updateJobName, steps);
+
+        service.triggerJobForUpdatedActiveEnrollments(programName, user, enrLookupTable, evnLookupTable, mappingObj);
+
+        verify(jobService, times(1)).triggerJob(programName, user, updateJobName, steps);
+        verify(updatedActiveEnrollmentWithEventsStep, times(1)).get(enrLookupTable, evnLookupTable, programName, mappingObj);
+    }
+
+    @Test
+    public void shouldLogErrorOnJobFailForUpdatedActiveEnrollments() throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, SyncFailedException {
+        when(updatedActiveEnrollmentWithEventsStep.get(enrLookupTable, evnLookupTable, programName, mappingObj)).thenReturn(step);
+        LinkedList<Step> steps = new LinkedList<>();
+        steps.add(step);
+        doThrow(new JobParametersInvalidException("Invalid Params")).when(jobService).triggerJob(programName, user, updateJobName, steps);
+
+        try {
+            service.triggerJobForUpdatedActiveEnrollments(programName, user, enrLookupTable, evnLookupTable, mappingObj);
+        } catch (Exception e) {
+            verify(jobService, times(1)).triggerJob(programName, user, updateJobName, steps);
+            verify(updatedActiveEnrollmentWithEventsStep, times(1)).get(enrLookupTable, evnLookupTable, programName, mappingObj);
+            verify(logger, times(1)).error("Active Enrollments: Invalid Params");
+        }
+
+    }
+
+}
