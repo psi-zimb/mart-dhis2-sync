e498e0b892f78a5ae52dc14abf1d239ddcde1285
Pratibhasagar V <pratibav@thoughtworks.com>
2018-12-12 11:19:18 +0530

    Pratibha | #141 | Add sqls and respective readers for New and Updated active enrollments with events.

---
+++ src/main/resources/sql/NewActiveEnrollmentWithEvents.sql
@@ -0,0 +1,23 @@
+SELECT enrTable.incident_date,
+       enrTable.date_created       AS enrollment_date_created,
+       enrTable.program_unique_id  AS program_unique_id,
+       enrTable.program            AS enrolled_program,
+       enrTable.enrollment_date    AS enr_date,
+       enrTable.status             AS enrollment_status,
+       enrTable."Patient_Identifier" AS enrolled_patient_identifier,
+       evnTable.*,
+       orgTracker.id               AS orgunit_id,
+       insTracker.instance_id
+FROM %s enrTable
+       LEFT JOIN %s evnTable ON evnTable."Patient_Identifier" = enrTable."Patient_Identifier" AND
+                                                      evnTable.enrollment_date = enrTable.enrollment_date
+       INNER JOIN instance_tracker insTracker ON insTracker.patient_id = enrTable."Patient_Identifier"
+       INNER JOIN orgunit_tracker orgTracker ON orgTracker.orgUnit = enrTable."OrgUnit"
+       LEFT JOIN enrollment_tracker enrTracker
+         ON enrTable.program = enrTracker.program AND enrTracker.instance_id = insTracker.instance_id
+              AND enrTracker.program_unique_id = enrTable.program_unique_id :: text
+WHERE enrTable.date_created :: TIMESTAMP > COALESCE((SELECT last_synced_date FROM marker WHERE category = 'enrollment'
+                                                                                           AND program_name = '%s'),
+                                                    '-infinity')
+  AND enrTracker.instance_id IS NULL
+  AND enrTable.status = 'ACTIVE';
