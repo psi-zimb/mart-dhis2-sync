dfda47637f5f2d9989eca9d69c57c657d08c613f
Mahitha Arveti <mahitha.arveti@gmail.com>
2018-12-26 15:37:39 +0530

    Mahitha | 000 | Adds tests cases

--- src/test/java/com/thoughtworks/martdhis2sync/trackerHandler/TrackersHandlerTest.java
+++ src/test/java/com/thoughtworks/martdhis2sync/trackerHandler/TrackersHandlerTest.java
@@ -5,6 +5,7 @@ import com.thoughtworks.martdhis2sync.model.EventTracker;
 import com.thoughtworks.martdhis2sync.util.EnrollmentUtil;
 import com.thoughtworks.martdhis2sync.util.EventUtil;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -100,6 +101,62 @@ public class TrackersHandlerTest {
     }
 
     @Test
+    public void shouldNotInsertEnrollmentsIntoEnrTrackerIfPrepareStatementIsFailed() throws SQLException {
+        String sql = "INSERT INTO public.enrollment_tracker(" +
+                "enrollment_id, instance_id, program, status, program_unique_id, created_by, date_created)" +
+                "values (?, ?, ?, ?, ?, ?, ?)";
+
+        when(dataSource.getConnection()).thenReturn(connection);
+        when(connection.prepareStatement(sql)).thenThrow(new SQLException("Could not prepareStatement"));
+
+        trackersHandler.insertInEnrollmentTracker("***REMOVED***", logPrefix, logger);
+
+        verify(dataSource, times(1)).getConnection();
+        verify(connection, times(1)).prepareStatement(sql);
+        verify(logger, times(1)).error(logPrefix + "Exception occurred while inserting Program " +
+                "Enrollment UIDs: Could not prepareStatement");
+    }
+
+    @Test
+    public void shouldNotInsertEnrollmentsIntoEnrollmentTrackerWhenExecuteUpdateIsFailed() throws SQLException {
+        String sql = "INSERT INTO public.enrollment_tracker(" +
+                "enrollment_id, instance_id, program, status, program_unique_id, created_by, date_created)" +
+                "values (?, ?, ?, ?, ?, ?, ?)";
+
+        EnrollmentAPIPayLoad apiPayLoad = new EnrollmentAPIPayLoad(
+                "enrId",
+                "instanceId",
+                "xhjKKwoq",
+                "jSsoNjesL",
+                "2019-10-12",
+                "2019-10-12",
+                "COMPLETED",
+                "1",
+                null
+        );
+
+        EnrollmentUtil.enrollmentsToSaveInTracker.add(apiPayLoad);
+
+        when(dataSource.getConnection()).thenReturn(connection);
+        when(connection.prepareStatement(sql)).thenReturn(preparedStatement);
+        when(preparedStatement.executeUpdate()).thenThrow(new SQLException("Could not execute update"));
+
+        trackersHandler.insertInEnrollmentTracker("***REMOVED***", logPrefix, logger);
+
+        verify(dataSource, times(1)).getConnection();
+        verify(connection, times(1)).prepareStatement(sql);
+        verify(preparedStatement, times(1)).setString(1, "enrId");
+        verify(preparedStatement, times(1)).setString(2, "instanceId");
+        verify(preparedStatement, times(1)).setString(3, "xhjKKwoq");
+        verify(preparedStatement, times(1)).setString(4, "COMPLETED");
+        verify(preparedStatement, times(1)).setString(5, "1");
+        verify(preparedStatement, times(1)).setString(6, "***REMOVED***");
+        verify(preparedStatement, times(1)).executeUpdate();
+        verify(logger, times(1)).error(logPrefix + "Exception occurred while inserting " +
+                "Program Enrollment UIDs: Could not execute update");
+    }
+
+    @Test
     public void shouldInsertEventIntoEventTracker() throws SQLException {
         String sql = "INSERT INTO public.event_tracker(" +
                 "event_id, instance_id, program, program_stage, event_unique_id, created_by, date_created)" +
@@ -145,6 +202,48 @@ public class TrackersHandlerTest {
         verify(connection, times(1)).prepareStatement(sql);
         verify(logger, times(1)).error(logPrefix + "Exception occurred while inserting Event " +
                 "UIDs: Could not prepareStatement");
+    }
+
+    @Test
+    public void shouldNoInsertEventsIntoEventTrackerWhenGettingConnectionIsFailed() throws SQLException {
+        when(dataSource.getConnection()).thenThrow(new SQLException("Could not get Database connection"));
+
+        trackersHandler.insertInEventTracker("***REMOVED***", logPrefix, logger);
+        verify(dataSource, times(1)).getConnection();
+        verify(logger, times(1)).error(logPrefix + "Exception occurred while inserting " +
+                "Event UIDs: Could not get Database connection");
+
+        EnrollmentUtil.enrollmentsToSaveInTracker.clear();
+    }
+
+    @Test
+    public void shouldNotInsertEventsIntoEventsTrackerWhenExecuteUpdateIsFailed() throws SQLException {
+        String sql = "INSERT INTO public.event_tracker(" +
+                "event_id, instance_id, program, program_stage, event_unique_id, created_by, date_created)" +
+                "values (?, ?, ?, ?, ?, ?, ?)";
+
+        List<EventTracker> eventTrackers = new LinkedList<>();
+        eventTrackers.add(new EventTracker("eventId1", "instanceId1", "xhjKKwoq", "1", "dkjfErjA"));
+        eventTrackers.add(new EventTracker("eventId2", "instanceId1", "xhjKKwoq", "2", "dkjfErjA"));
+        EventUtil.eventsToSaveInTracker = eventTrackers;
+
+        when(dataSource.getConnection()).thenReturn(connection);
+        when(connection.prepareStatement(sql)).thenReturn(preparedStatement);
+        when(preparedStatement.executeUpdate()).thenThrow(new SQLException("Could not execute update"));
+
+        trackersHandler.insertInEventTracker("***REMOVED***", logPrefix, logger);
+
+        verify(dataSource, times(1)).getConnection();
+        verify(connection, times(1)).prepareStatement(sql);
+        verify(preparedStatement, times(1)).setString(1, "eventId1");
+        verify(preparedStatement, times(1)).setString(2, "instanceId1");
+        verify(preparedStatement, times(1)).setString(3, "xhjKKwoq");
+        verify(preparedStatement, times(1)).setString(4, "dkjfErjA");
+        verify(preparedStatement, times(1)).setString(5, "1");
+        verify(preparedStatement, times(1)).setString(6, "***REMOVED***");
+        verify(preparedStatement, times(1)).executeUpdate();
+        verify(logger, times(1)).error(logPrefix + "Exception occurred while inserting " +
+                "Event UIDs: Could not execute update");
     }
 
     @Test
@@ -217,6 +316,62 @@ public class TrackersHandlerTest {
         verify(preparedStatement, times(1)).executeUpdate();
         verify(logger, times(1)).error(logPrefix + "Exception occurred while updating " +
                 "Program Enrollment UIDs: Could not execute update");
+    }
+
+    @Test
+    public void shouldNoUpdateEnrollmentsIntoEnrollmentTrackerWhenGettingConnectionIsFailed() throws SQLException {
+        when(dataSource.getConnection()).thenThrow(new SQLException("Could not get Database connection"));
+
+        trackersHandler.updateInEnrollmentTracker("***REMOVED***", logPrefix, logger);
+        verify(dataSource, times(1)).getConnection();
+        verify(logger, times(1)).error(logPrefix + "Exception occurred while updating " +
+                "Program Enrollment UIDs: Could not get Database connection");
+    }
+
+    @Test
+    public void shouldNotUpdateEnrollmentsIntoEnrTrackerIfPrepareStatementIsFailed() throws SQLException {
+        String sql = "UPDATE public.enrollment_tracker " +
+                "SET status = ?, created_by = ?, date_created = ? " +
+                "WHERE enrollment_id = ?";
+
+        when(dataSource.getConnection()).thenReturn(connection);
+        when(connection.prepareStatement(sql)).thenThrow(new SQLException("Could not prepareStatement"));
+
+        trackersHandler.updateInEnrollmentTracker("***REMOVED***", logPrefix, logger);
+
+        verify(dataSource, times(1)).getConnection();
+        verify(connection, times(1)).prepareStatement(sql);
+        verify(logger, times(1)).error(logPrefix + "Exception occurred while updating Program " +
+                "Enrollment UIDs: Could not prepareStatement");
+    }
+
+    @Test
+    public void shouldClearTrackers() {
+        EnrollmentAPIPayLoad apiPayLoad = new EnrollmentAPIPayLoad(
+                "enrId",
+                "instanceId",
+                "xhjKKwoq",
+                "jSsoNjesL",
+                "2019-10-12",
+                "2019-10-12",
+                "COMPLETED",
+                "1",
+                null
+        );
+
+        LinkedList<EnrollmentAPIPayLoad> apiPayLoads = new LinkedList<>();
+        apiPayLoads.add(apiPayLoad);
+        EnrollmentUtil.enrollmentsToSaveInTracker = apiPayLoads;
+
+        List<EventTracker> eventTrackers = new LinkedList<>();
+        eventTrackers.add(new EventTracker("eventId1", "instanceId1", "xhjKKwoq", "1", "dkjfErjA"));
+        eventTrackers.add(new EventTracker("eventId2", "instanceId1", "xhjKKwoq", "2", "dkjfErjA"));
+        EventUtil.eventsToSaveInTracker = eventTrackers;
+
+        TrackersHandler.clearTrackerLists();
+
+        Assert.assertEquals(0, EnrollmentUtil.enrollmentsToSaveInTracker.size());
+        Assert.assertEquals(0, EventUtil.eventsToSaveInTracker.size());
     }
 
     @After
